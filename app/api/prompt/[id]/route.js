c92951c99d189e6f2702cd759e772cf312838dc9
SRIVATSAN S <95332677+vatsan-0613@users.noreply.github.com>
2024-02-29 20:35:17 +0530

    first-commit

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,41 @@
+import { connectToDB } from '@utils/database';
+import Prompt from '@models/prompt';
+
+export const GET = async (request, {params}) => {
+    try{
+        await connectToDB();
+        const prompt = await Prompt.findById(params.id).populate('creator');
+        if(!prompt) return new Response("Prompt Not Found", {status : 404})
+        return new Response(JSON.stringify(prompt), {status : 200})
+    }catch(error){
+        return new Response("Failed to fetch prompts", {status : 500})
+    }
+}
+
+export const PATCH = async(request, {params}) => {
+    const {prompt, tag} = await request.json();
+    try{
+        await connectToDB();
+        const existingPrompt = await Prompt.findById(params.id);
+        if(!existingPrompt) return new Response("Prompt Not found", {status : 404})
+
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+        return new Response(JSON.stringify(existingPrompt), {status : 200})
+    }catch(error){
+        return new Response("Failed to update prompt", {status : 404})
+    }
+}
+
+export const DELETE = async(request, {params}) => {
+    try{
+        console.log("id : "+params.id)
+        await connectToDB();
+        await Prompt.findOneAndDelete({ _id: params.id });
+        return new Response("Prompt Deleted Successfully", {status : 200})
+    } catch(error){
+        return new Response("Failed to delete Prompt", {status : 500})
+    }
+}
---
+++ models/prompt.js
@@ -0,0 +1,22 @@
+import mongoose, {Schema, model} from 'mongoose';
+
+const PromptSchema = new Schema({
+    creator:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User'
+    }, 
+    prompt : {
+        type : String, 
+        required : [true, 'Prompt is required.'],
+    },
+    tag : {
+        type : String, 
+        required : [true, 'Tag is required.'],
+    }
+})
+
+const models = mongoose.models || {};
+
+const Prompt = models.Prompt || model('Prompt', PromptSchema); 
+
+export default Prompt;
