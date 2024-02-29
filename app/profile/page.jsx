a28ad6d882ce9e9297f7c113e531da1cdad91b31
SRIVATSAN S <95332677+vatsan-0613@users.noreply.github.com>
2024-02-29 20:39:21 +0530

    fix-typo

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState} from 'react'
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import Profile from '@components/profile';
+import Profile from '@components/Profile';
 
 const MyProfile = () => {
     const router = useRouter();
